faa0400791ced6aab477caa682ddb9dc07d16997
Apichaya-Jithaisong <17041.mnr@gmail.com>
2023-02-10 22:54:14 +0700

    textLogin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 
-import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/navbar';
-import { BrowserRouter as Router} from 'react-router-dom';
-import Home from './pages/home';
-import About from './pages/about';
-import Question from './pages/question';
+// import { NavLink, Route, Routes } from 'react-router-dom';
+// import Navbar from './components/navbar';
+// import { BrowserRouter as Router} from 'react-router-dom';
+// import Home from './pages/home';
+// import About from './pages/about';
+// import Question from './pages/question';
+import Login from './pages/login';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={<Home/>}> </Route>
-        <Route path="/about" exact element={<About/>}> </Route>
-        <Route path="/question" exact element={<Question/>}> </Route>
-      </Routes>
-    </Router>
+    <Login/>
+    // <Router>
+    //   <Navbar />
+    //   <Routes>
+    //     <Route path="/" exact element={<Home/>}> </Route>
+    //     <Route path="/about" exact element={<About/>}> </Route>
+    //     <Route path="/question" exact element={<Question/>}> </Route>
+    //   </Routes>
+    // </Router>
   );
 }
 
