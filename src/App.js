8a79f32fcc563b78ddc7d10d134c8b14de46f6b1
Apichaya-Jithaisong <17041.mnr@gmail.com>
2023-03-03 02:32:19 +0700

    notification-last

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from './components/navbar';
 import { BrowserRouter as Router} from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
-import Question from './pages/question';
+// import Question from './pages/question';
 import Login from './pages/registration/login';
 import SignUp from './pages/registration/signup';
 import Authenticate from './pages/registration/authenticate';
@@ -20,7 +20,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<Notification/>}> </Route>
         <Route path="/about" exact element={<About/>}> </Route>
-        <Route path="/question" exact element={<Question/>}> </Route>
+        {/* <Route path="/question" exact element={<Question/>}> </Route> */}
       </Routes>
     </Router>
   );
--- src/pages/notification.js
+++ src/pages/notification.js
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
 import "../styles/notification.css";
-import { Tabs, Tab, Dropdown } from "react-bootstrap";
+import { Tabs, Tab, Dropdown, ListGroup } from "react-bootstrap";
+import {
+  Avatar,
+  List,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { classes } from "istanbul-lib-coverage";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
 
 const Review = () => {
   //Search Bar
@@ -20,263 +30,221 @@ const Review = () => {
   //-----------
 
   return (
-    <div>
-      <div className="pagePadding">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8">
-              <div style={{ position: "relative" }}>
-                <Tabs style={{ width: "70%" }} defaultActiveKey="all">
-                  <Tab
-                    className="pt-4 tab-detail"
-                    eventKey="all"
-                    title="ทั้งหมด"
-                  >
-                    <div className="row">
-                      <div className="col-9">
-                        <div
-                          className="body"
-                          style={{
-                            display: "flex",
-                          }}
-                        >
-                          <div className="box">
-                            <div className="profile-image">
-                              <img
-                                src={require("../images/home/main.png")}
-                                alt="main page png"
-                                className="img-fluid"
-                              />
-                            </div>
-                          </div>
-                          <div
-                            className="box"
-                            style={{
-                              display: "flex",
-                              alignItems: "center",
-                            }}
-                          >
-                            วรรณดา แม็กซิมอฟ
-                            <span style={{ paddingLeft: "1rem" }}>
-                              4 วันที่ผ่านมา
-                            </span>
-                          </div>
-
-                          <div style={{ paddingLeft: "1rem" }}></div>
-                        </div>
-                        <div>
-                          <div className="homeHeader2">
-                            วิชานี้ GEN231 ดีมั้ยครับ อยากฟังรีวิวจากพี่ๆ ครับ
-                          </div>
-                          <div className="text-limit body">
-                            ผมเรียนอยู่ ปี 3 ภาควิชาคณิตศาสตร์ครับ
-                            อยากทราบความเห็นของพี่ๆ ที่เรียนวิชานี้ปีที่แล้วครับ
-                            ว่ามันดีไม่ดีอย่าง ไร มันดึงเกรดเยอะมั้ยครับ
-                            พอดีไม่อยากให้เกรดตกครับผม
-                          </div>
-                          <div style={{ paddingTop: "1rem", width: "100%" }}>
-                            <div className="box">
-                              <div className="rectangle-container">
-                                <div className="rectangle-border">
-                                  <div className="rectangle-text">GEN231</div>
-                                </div>
-                              </div>
-                            </div>
-                            <div className="box float-end">
-                              <div
-                                style={{
-                                  display: "flex",
-                                  alignItems: "center",
-                                }}
-                              >
-                                <div style={{ flex: 1 }}>
-                                  <span style={{ paddingRight: "0.5rem" }}>
-                                    <img
-                                      src={
-                                        require("../images/icon/chat.svg")
-                                          .default
-                                      }
-                                      alt="chat svg"
-                                    />
-                                  </span>
-                                  <span style={{ paddingRight: "1rem" }}>
-                                    12
-                                  </span>
-                                  <span style={{ paddingRight: "0.5rem" }}>
-                                    <img
-                                      src={
-                                        require("../images/icon/like.svg")
-                                          .default
-                                      }
-                                      alt="like svg"
-                                    />
-                                  </span>
-                                  <span style={{ paddingRight: "1rem" }}>
-                                    512
-                                  </span>
-                                </div>
-                                <Dropdown drop="down">
-                                  <Dropdown.Toggle
-                                    variant="link"
-                                    id="dropdown-basic"
-                                    style={{
-                                      border: "none",
-                                      boxShadow: "none",
-                                      color: "transparent",
-                                    }}
-                                  >
-                                    <span style={{ color: "black" }}>
-                                      &bull;&bull;&bull;
-                                    </span>
-                                  </Dropdown.Toggle>
-
-                                  <Dropdown.Menu>
-                                    <Dropdown.Item href="#">
-                                      Option 1
-                                    </Dropdown.Item>
-                                    <Dropdown.Item href="#">
-                                      Option 2
-                                    </Dropdown.Item>
-                                    <Dropdown.Item href="#">
-                                      Option 3
-                                    </Dropdown.Item>
-                                  </Dropdown.Menu>
-                                </Dropdown>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-
-                      <div className="col-3">
-                        <img
-                          src={require("../images/example/1x1.png")}
-                          alt="ex_1x1"
-                          className="img-fluid float-end"
-                        />
-                      </div>
-                      <div style={{ paddingTop: "1rem" }}>
-                        <hr />
-                      </div>
-                    </div>
-                  </Tab>
-
-                  <Tab className="pt-5" eventKey="subject" title="วิชาเรียน">
-                    ่าาาาาา
-                  </Tab>
-                  <Tab className="pt-5" eventKey="teacher" title="อาจารย์">
-                    ่าาาาาา
-                  </Tab>
-                  <Tab className="pt-5" eventKey="restaurant" title="ร้านอาหาร">
-                    ่าาาาาา
-                  </Tab>
-                  <Tab className="pt-5" eventKey="dorm" title="หอพัก">
-                    ่าาาาาา
-                  </Tab>
-                  <Tab className="pt-5" eventKey="work" title="สถานที่ฝึกงาน">
-                    ่าาาาาา
-                  </Tab>
-                </Tabs>
-                <div style={{ position: "absolute", top: 0, right: 0 }}>
-                  <Dropdown>
-                    <Dropdown.Toggle
-                      style={{
-                        backgroundColor: "transparent",
-                        color: "BLACK",
-                        borderColor: "black",
-                        width: "100px",
-                      }}
-                      id="dropdown-basic"
-                    >
-                      {selectedOption}
-                    </Dropdown.Toggle>
-
-                    <Dropdown.Menu>
-                      <Dropdown.Item
-                        onClick={() => handleOptionSelect("ทั้งหมด")}
-                      >
-                        ทั้งหมด
-                      </Dropdown.Item>
-                      <Dropdown.Item
-                        onClick={() => handleOptionSelect("ยอดนิยม")}
-                      >
-                        ยอดนิยม
-                      </Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </div>
-              </div>
-              <div className="box float-end">
-                <div className="float-end"></div>
-              </div>
-            </div>
-            <div className="col-md-1">
-              <div className="vertical-line"></div>
-            </div>
-            <div className="col-md-3">
-              <button
-                type="button"
-                className="button"
-                onClick=""
-                style={{ width: "100%" }}
-              >
-                เริ่มต้นการเขียนโพสต์
-              </button>
-              <div className="searchBar">
-                <div className="search-container">
-                  <input
-                    type="text"
-                    value={query}
-                    onChange={handleInputChange}
-                    style={{
-                      height: "2.5rem",
+    <div className="page">
+      <div className="row">
+        <div className="col-md vertLine">
+          <div className="left-content">
+            <span className="qTitle">การแจ้งเตือน</span>
+            <Tabs defaultActiveKey="all" className="Qtabs pt-4 ">
+              <Tab className="pt-4" eventKey="all" title="ทั้งหมด">
+                <div>
+                  <List
+                    sx={{
                       width: "100%",
-                      borderColor: "#E6E6E6",
+                      maxWidth: "100%",
+                      bgcolor: "background.paper",
                     }}
-                  />
-                  <div className="search-inside">
-                    <div>
-                      <span>
-                        <img
-                          src={require("../images/icon/search.svg").default}
-                          alt="search svg"
+                  >
+                    <ListItem alignItems="flex-start">
+                      
+                      <ListItemAvatar>
+                        <Avatar
+                          alt="Remy Sharp"
+                          src="/static/images/avatar/1.jpg"
                         />
-                      </span>
-                      <span style={{ paddingLeft: "1rem", fontSize: "14px" }}>
-                        Search Mod's Talk
-                      </span>
+                      </ListItemAvatar>
+                      <ListItemText 
+                      
+                        primary={
+                          <React.Fragment>
+                            <Typography
+                              className="notiName"
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              วรรณดา แม็กซิมอฟ
+                            </Typography>
+                            &nbsp;
+                            &nbsp;
+                            <Typography
+                              className="notiText"
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              ได้แสดงความคิดเห็นต่อโพสต์
+                            </Typography>
+                            &nbsp;
+                            &nbsp;
+                            <Typography
+                              className="notiPost"
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              วิชานี้ GEN231 ดีมั้ยครับ อยากฟังรีวิจากพี่ๆ ครับ
+                            </Typography>
+                          </React.Fragment>
+                        }
+                      />
+                    </ListItem>
+                  </List>
+                  {/* <p className="poster-name pb-3">ผู้ไม่ประสงค์ออกนาม</p>
+                  <p className="pb-2">
+                    ผมสงสัยครับ รบกวนผู้รู้ตอบผมทีว่า วิทยคอมของคณะ IT
+                    กับคณะวิทย์ต่างกันยังไงครับ
+                  </p>
+                  <div className="post-detail row">
+                    <div className="col-6">
+                      <span className="post-date">10 มกราคม 2565</span>
                     </div>
+
+                    <div className="col menu-group">
+                      <div className="row">
+                        <div className="col sub-menu">
+                          <a href="" className="post-comment">
+                            <img className="menu-pic"
+                              src={
+                                require("../images/question/chat_1.svg").default
+                              }
+                              alt=""
+                            />
+                            <text id="comment-count">10</text>
+                            
+
+                          </a>
+
+                        </div>
+
+                        <div className="col-2 sub-menu menu-dropdown">
+
+                        <Dropdown>
+      <Dropdown.Toggle variant="link" id="question-dropdown">
+      <img
+                            className="menu-dropdown"
+                            src={
+                              require("../images/question/three_dots.svg")
+                                .default
+                            }
+                            alt=""
+                          />
+                        
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu >
+        <Dropdown.Item href="#/action-1">Report Post</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Delete Post</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu >
+    </Dropdown>    
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div> */}
+                </div>
+              </Tab>
+
+              <Tab className="pt-5" eventKey="no-ans" title="การตอบกลับ">
+                ่าาาาาา
+              </Tab>
+            </Tabs>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="d-grid mx-auto right-content ">
+            <button
+              type="button"
+              class="btn ask-button"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+            >
+              เริ่มต้นถามคำถาม
+            </button>
+            {/* <!-- Modal --> */}
+            <div
+              class="modal fade"
+              id="exampleModal"
+              tabindex="-1"
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">
+                      Modal title
+                    </h1>
+                    <button
+                      type="button"
+                      class="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div class="modal-body">...</div>
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      Close
+                    </button>
+                    <button type="button" class="btn btn-primary">
+                      Save changes
+                    </button>
                   </div>
                 </div>
               </div>
-              <div style={{ paddingTop: "1rem", color: "#4F4F4F" }}>
-                <span>คลิก</span>{" "}
-                <span>
+            </div>
+
+            <div className="tips ">
+              <div class="row tipsTitle">
+                <div class="col ">เคล็ดลับเครื่องมือ</div>
+
+                <div class="col-2 close-button">
                   <img
-                    src={require("../images/icon/search.svg").default}
-                    alt="search svg"
-                    style={{ color: "#4F4F4F" }}
+                    className=""
+                    src={require("../images/question/cross.svg").default}
+                    alt=""
                   />
-                </span>{" "}
-                <span>
-                  และพิมพ์เกี่ยวกับสิ่งที่คุณอยากรู้เพื่อค้นหาสิ่งที่คุณต้องการอ่าน
-                  เริ่มต้นเพลิดเพลินไปกับการรีวิวด้านบน
-                </span>
-              </div>
-              <div style={{ paddingTop: "1rem" }}>
-                <hr />
-              </div>
-              <div
-                className="body"
-                style={{ paddingTop: "1rem", paddingBottom: "1rem" }}
-              >
-                เป็นทิ่นิยมใน Mod's Talk
-              </div>
-              <div className="box">
-                <div className="rectangle-border">
-                  <div className="rectangle-text">Programming</div>
                 </div>
               </div>
+
+              <p className="tipsContent">
+                {" "}
+                คุณสามารถเริ่มถามคำถามจากปุ่มด้านบนนี้ หรือ
+                ถ้าสงสัยเกี่ยวกับรายวิชา หอพัก สิ่งต่างๆ
+                สามารถค้นหาสิ่งที่คุณอยากรู้ได้ที่แถบด้านล่างนี้เลย
+              </p>
+            </div>
+
+            <div className="search-box">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="ค้นหาคำถาม"
+              ></input>
+            </div>
+
+            <div className="search-tips">
+              <text className="search-tips" id="s1">
+                คลิก
+              </text>
+              <img
+                className="search-icon"
+                id="2"
+                src={require("../images/question/search.svg").default}
+                alt=""
+              />
+
+              <text className="search-tips" id="s2">
+                และพิมพ์เกี่ยวกับสิ่งที่คุณอยากรู้เพื่อค้นหาคำถาม
+                ที่ใกล้เคียงหรือหาคำตอบไปด้วยกัน
+              </text>
             </div>
           </div>
         </div>
