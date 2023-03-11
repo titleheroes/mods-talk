42612e3bfdbe432da81ef8b6f0a4678e2cb07f42
titleheroes <title13260@gmail.com>
2023-03-11 21:11:22 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Question from "./pages/question";
-import Review from "./pages/review/review copy";
-import Post from "./pages/review/post";
+
 import Login from "./pages/registration/login";
 import SignUp from "./pages/registration/signup";
 import ForgetPass from "./pages/registration/forgetPass";
 import ForgetPassSent from "./pages/registration/forgetPassSent";
 import Authenticate from "./pages/registration/authenticate";
 import DataUser from "./pages/registration/datauser";
+
+import Review from "./pages/review/review copy";
+import Post from "./pages/review/post";
 
 function App() {
   return (
@@ -32,6 +34,8 @@ function App() {
         ></Route>
         <Route path="/authenticate" exact element={<Authenticate />}></Route>
         <Route path="/datauser" exact element={<DataUser />}></Route>
+        <Route path="/review" exact element={<Review />}></Route>
+        <Route path="/post" exact element={<Post />}></Route>
       </Routes>
     </Router>
   );
