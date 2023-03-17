c07869d9f1b39cb40d59338f98c9ea36619e3ad8
JakkapatKan <sukee0000@gmail.com>
2023-03-18 04:22:40 +0700

    admin sidebar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./sidebar.css";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import ReceiptIcon from "@mui/icons-material/Receipt";
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
+
       <div className="top">
         <img className="logo" src={require("../../images/logo2.png")} alt="" />
       </div>
@@ -13,21 +15,45 @@ const Sidebar = () => {
       <div className="center">
         <ul>
           <li>
-            <span>
-              <AccountBoxIcon className="userIcon" />
-              ผู้ใช้งาน
-            </span>
+            <div className="sidetabs" id="side1">
+                <span>
+                <AccountBoxIcon className="userIcon me-2" />
+                ผู้ใช้งาน
+                </span>
+
+            </div>
+            
           </li>
           <li>
-            <span>
-              <ReceiptIcon />
-              โพสต์
-            </span>
+            <div className="sidetabs" id="side2">
+                    <span>
+                    <ReceiptIcon className="userIcon me-2" />
+                    โพสต์
+                    </span>
+
+                </div>
           </li>
         </ul>
       </div>
 
-      <div className="bottom">option</div>
+      <div className="bottom">
+        
+        <img
+            src={require("../../images/home/main.png")}
+            alt="Review.png"
+            className="img-fluid prof-pic rounded-circle shadow-4 me-2"
+        />
+        <div className="profile-detail">
+            <span className="profile-name">กอร์ดอน แรมซีย์ <p className="profile-role ms-2">• แอดมิน</p> </span> 
+            
+        </div>
+
+        <button className="logout-button p-2"><LogoutIcon className="me-2"  />ออกจากระบบ</button>
+        
+        
+      </div>
+
+
     </div>
   );
 };
