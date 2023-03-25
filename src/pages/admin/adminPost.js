695e2ff7fc2f0c205fa7b8c52c708659ec20e4ed
JakkapatKan <sukee0000@gmail.com>
2023-03-25 16:15:30 +0700

    table pagination

--- src/pages/admin/adminPost.js
+++ src/pages/admin/adminPost.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import "../../styles/admin.css";
 import Sidebar from "../../components/sidebar/sidebar";
 
@@ -14,10 +14,93 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+
+
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import { createTheme, ThemeProvider } from '@mui/material';
+
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import SearchIcon from '@mui/icons-material/Search';
 
-import { createTheme, ThemeProvider } from '@mui/material';
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
 const theme = createTheme({
     typography: {
     
@@ -48,7 +131,23 @@ function createData(id, msg, fullname,post_date,post_status,report_count) {
 
 
 
-const adminPost = () => {
+const AdminPost = () => {
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
   return (
     <div className='adminpage'>
         <Sidebar/>
@@ -90,8 +189,10 @@ const adminPost = () => {
                         <TableCell align="left">การจัดการ</TableCell>
                     </TableRow>
                     </TableHead>
-                    <TableBody>
-                    {rows.map((row) => (
+                    <TableBody>{(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row) => (
                         <TableRow
                         key={row.id}
                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -107,6 +208,48 @@ const adminPost = () => {
                         </TableRow>
                     ))}
                     </TableBody>
+                    <TableFooter>
+          <TableRow>
+            <TablePagination 
+             sx={{
+              '.MuiTablePagination-selectLabel': {
+                marginBottom: '0rem'
+                
+              },
+
+              '.MuiTablePagination-displayedRows': {
+                marginBottom: '0rem'
+                
+              },
+
+              '.MuiTablePagination-toolbar': {
+                
+                borderTop: '1px solid #dedede'
+                
+              },
+
+            }}
+      
+              rowsPerPageOptions={[5, 10, 20, { label: 'ทั้งหมด', value: -1 }]}
+              
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'จำนวนรายการต่อหน้า',
+                },
+                native: true,
+              }}
+              
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+                    
                     </Table>
                 </TableContainer>
 
@@ -145,7 +288,10 @@ const adminPost = () => {
                     </TableRow>
                     </TableHead>
                     <TableBody>
-                    {rows.map((row) => (
+                    {(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row) => (
                         <TableRow
                         key={row.id}
                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -162,6 +308,47 @@ const adminPost = () => {
                         </TableRow>
                     ))}
                     </TableBody>
+                    <TableFooter>
+          <TableRow>
+            <TablePagination 
+             sx={{
+              '.MuiTablePagination-selectLabel': {
+                marginBottom: '0rem'
+                
+              },
+
+              '.MuiTablePagination-displayedRows': {
+                marginBottom: '0rem'
+                
+              },
+
+              '.MuiTablePagination-toolbar': {
+                
+                borderTop: '1px solid #dedede'
+                
+              },
+
+            }}
+      
+              rowsPerPageOptions={[5, 10, 20, { label: 'ทั้งหมด', value: -1 }]}
+              
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'จำนวนรายการต่อหน้า',
+                },
+                native: true,
+              }}
+              
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
                     </Table>
                 </TableContainer>
 
@@ -187,4 +374,4 @@ const adminPost = () => {
   )
 }
 
-export default adminPost
+export default AdminPost
--- src/pages/admin/adminUser.js
+++ src/pages/admin/adminUser.js
@@ -22,13 +22,86 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 
-import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+
+
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+
 
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import SearchIcon from '@mui/icons-material/Search';
 
 import { createTheme, ThemeProvider } from '@mui/material';
+
+
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
 
 const theme = createTheme({
     typography: {
@@ -111,7 +184,25 @@ function createData(id, code, fullname, faculty, major,email,regis_date,manage) 
 
 
 
-const adminUser = () => {
+const AdminUser = () => {
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+
   return (
     <div className='adminpage'>
         <Sidebar/>
@@ -138,12 +229,11 @@ const adminUser = () => {
               <div className='muitable px-4 pb-5'> 
 
               <ThemeProvider theme={theme}>
-              <TableContainer className='' component={Paper}>
 
 
-                <Table sx={{ minWidth: 650 }} aria-label="a dense table"> {/*add [size="small"] this line to dense*/}
-
-                    <TableHead>
+              <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+      <TableHead>
                     <TableRow>
                         <TableCell align="center">ID</TableCell>
                         <TableCell align="left">รหัสนักศึกษา</TableCell>
@@ -155,9 +245,12 @@ const adminUser = () => {
                         <TableCell align="left">การจัดการ</TableCell>
                     </TableRow>
                     </TableHead>
-                    <TableBody>
-                    {rows.map((row) => (
-                        <TableRow
+        <TableBody>
+          {(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row) => (
+            <TableRow
                         key={row.id}
                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                         >
@@ -172,12 +265,62 @@ const adminUser = () => {
                         <TableCell align="left">{row.regis_date}</TableCell>
                         <TableCell align="center"><AlertDialogSlide name={row.fullname} /></TableCell>
                         </TableRow>
-                    ))}
-                    </TableBody>
-                    </Table>
-                </TableContainer>
+          ))}
+
+          {/* {emptyRows > 0 && (
+            <TableRow style={{ height: 50 * emptyRows }}>
+              
+            </TableRow>
+          )} */}
+
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination 
+             sx={{
+              '.MuiTablePagination-selectLabel': {
+                marginBottom: '0rem'
+                
+              },
+
+              '.MuiTablePagination-displayedRows': {
+                marginBottom: '0rem'
+                
+              },
+
+              '.MuiTablePagination-toolbar': {
+                
+                borderTop: '1px solid #dedede'
+                
+              },
+
+            }}
+      
+              rowsPerPageOptions={[5, 10, 20, { label: 'ทั้งหมด', value: -1 }]}
+              
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'จำนวนรายการต่อหน้า',
+                },
+                native: true,
+              }}
+              
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
 
               </ThemeProvider>
+
+              
 
               
               
@@ -211,9 +354,11 @@ const adminUser = () => {
 
             </Tabs>
 
-
+            
 
         </div>
+
+        
 
 
     </div>
@@ -221,4 +366,4 @@ const adminUser = () => {
   )
 }
 
-export default adminUser
+export default AdminUser
