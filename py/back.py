b2c095b37dd1f155c6fff8ff5e9a8cc9592cc1a3
JakkapatKan <sukee0000@gmail.com>
2023-05-15 19:54:22 +0700

    online sentiment

--- py/back.py
+++ py/back.py
@@ -10,9 +10,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def test():
--- src/pages/Question/test.js
+++ src/pages/Question/test.js
@@ -1,37 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect,useState } from 'react';
 import axios from 'axios';
+
+
+const api_address = 'http://jakkapatkan.pythonanywhere.com/api/sentiment'
 
 const MyComponent = () => {
   const [inputValue, setInputValue] = useState('');
+  
+
+
+  /*----------------------- Old Version --------------------*/
+
+  // const sendStringToFlask = () => {
+  //   axios.post('http://127.0.0.1:5000/api/sentiment', { string: inputValue })
+  //     .then(response => {
+  //       setResult(response.data.result); // Extract the result from the response
+  //       console.log(response.data.result)
+  //     })
+  //     .catch(error => {
+  //       // Handle any errors that occur during the request
+  //     });
+  // };
+
+  /*----------------------- New Version --------------------*/
+  const [inputText, setInputText] = useState('');
   const [result, setResult] = useState('');
 
-  const sendStringToFlask = () => {
-    axios.post('/api/sentiment', { string: inputValue })
-      .then(response => {
-        setResult(response.data.result); // Extract the result from the response
-        console.log(response.data.result)
-      })
-      .catch(error => {
-        // Handle any errors that occur during the request
+  const TestsendDataToFlask = async () => {
+    try {
+      const response = await axios.post(api_address, {
+        text: inputText,
+
       });
+      setResult(response.data.result);
+      console.log('result => '+response.data.result);
+    } catch (error) {
+      console.error(error);
+    }
   };
+
 
  
   return (
     <div>
-        <div>   
-
-            <textarea
-                type="text"
-                className='form-control mt-3 '
-                value={inputValue}
-                onChange={e => setInputValue(e.target.value)}
-            />
-
-            <button onClick={sendStringToFlask} className='btn btn-primary mt-3'>Send to Flask</button>
-            <p className='pt-3'>Result from Flask: {result}</p>
-
+        <div>
+          <input type="text" value={inputText} onChange={(e) => setInputText(e.target.value)} />
+          <button onClick={TestsendDataToFlask}>Send to Flask</button>
+          <p>Response from Flask: {result}</p>
         </div>
+
+
       
       
     </div>
